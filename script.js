57939aa257e707bb2c14a1f92c528162482b67ce
Hitarthpareek <80564705+Hitarthpareek@users.noreply.github.com>
2026-01-07 19:58:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+let audio; // will store current playing sound
+
+document.getElementById("applause").onclick = function () {
+  audio = new Audio("sounds/applause.mp3");
+  audio.play();
+};
+
+document.getElementById("boo").onclick = function () {
+  audio = new Audio("sounds/boo.mp3");
+  audio.play();
+};
+
+document.getElementById("gasp").onclick = function () {
+  audio = new Audio("sounds/gasp.mp3");
+  audio.play();
+};
+
+document.getElementById("tada").onclick = function () {
+  audio = new Audio("sounds/tada.mp3");
+  audio.play();
+};
+
+document.getElementById("victory").onclick = function () {
+  audio = new Audio("sounds/victory.mp3");
+  audio.play();
+};
+
+document.getElementById("wrong").onclick = function () {
+  audio = new Audio("sounds/wrong.mp3");
+  audio.play();
+};
+
+document.querySelector(".stop").onclick = function () {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+};
