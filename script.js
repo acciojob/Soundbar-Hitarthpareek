95b7e699dea0cab2f1367eea45e9c9e8fd585d6f
Hitarthpareek <80564705+Hitarthpareek@users.noreply.github.com>
2026-01-07 21:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-let audio; // will store current playing sound
-
+let audio = document.createElement("audio");
+document.body.appendChild(audio);
+ 
 document.getElementById("applause").onclick = function () {
-  audio = new Audio("sounds/applause.mp3");
+  audio.src = "sounds/applause.mp3";
   audio.play();
 };
 
 document.getElementById("boo").onclick = function () {
-  audio = new Audio("sounds/boo.mp3");
+  audio.src = "sounds/boo.mp3";
   audio.play();
 };
 
 document.getElementById("gasp").onclick = function () {
-  audio = new Audio("sounds/gasp.mp3");
+  audio.src = "sounds/gasp.mp3";
   audio.play();
 };
 
 document.getElementById("tada").onclick = function () {
-  audio = new Audio("sounds/tada.mp3");
+  audio.src = "sounds/tada.mp3";
   audio.play();
 };
 
 document.getElementById("victory").onclick = function () {
-  audio = new Audio("sounds/victory.mp3");
+  audio.src = "sounds/victory.mp3";
   audio.play();
 };
 
 document.getElementById("wrong").onclick = function () {
-  audio = new Audio("sounds/wrong.mp3");
+  audio.src = "sounds/wrong.mp3";
   audio.play();
 };
 
 document.querySelector(".stop").onclick = function () {
-  if (audio) {
-    audio.pause();
-    audio.currentTime = 0;
-  }
+  audio.pause();
+  audio.currentTime = 0;
 };
